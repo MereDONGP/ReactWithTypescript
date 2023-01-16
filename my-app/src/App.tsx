0a184260c5aecc5e272c803d8ab2775ffbea6dd6
Don <pullend15@gmail.com>
2023-01-16 16:15:45 -0500

    adding in the types

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,27 +7,66 @@ import Total from './components/Total';
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+  
+  interface CourseNormalPart extends CoursePartBase {
+    type: "normal";
+    description: string;
+  }
+  
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+  
+  interface CourseSubmissionPart extends CoursePartBase {
+    type: "submission";
+    description: string;
+    exerciseSubmissionLink: string;
+  }
+  
+  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the easy course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the hard course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
     }
-  ];
+  ]
+
+  
 
   return (
     <div>
       <Header nmae={courseName}></Header>
       <Content data={courseParts}></Content>
       <Total data={courseParts}></Total>
-        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
     </div>
   );
 };
